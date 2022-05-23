f4ecc773b5ea657e10f18bd87754ef6a05cfd5ef
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2022-05-23 13:58:41 +0200

    Implement function to handle gallery API request

--- src/app/gallery.js
+++ src/app/gallery.js
@@ -5,7 +5,27 @@ const User = require('./models/user.js'); // get our mongoose model
 
 router.get("", async (req, res) => {
     
-	res.send();
+	// Thanks to tokenChecker(), we can assume that the user is authenticated
+
+	// Get all users from DB
+	let users = await User.find().populate('pictures');
+
+	console.log(users);
+
+	pictures = [];
+
+	users.forEach( (user, u_index) => {
+		users[u_index].pictures.forEach((picture, p_index) => {
+			users[u_index].pictures[p_index].user = user.email
+			pictures.push(users[u_index].pictures[p_index]);
+		});
+	});
+	pictures.sort( (a, b) => {
+		return b.time - a.time;
+	});
+	console.log(pictures);
+
+	res.json(pictures);
 });
 
 module.exports = router;
