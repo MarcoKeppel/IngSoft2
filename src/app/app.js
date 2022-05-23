47272b9076329d362ca0794aa47c3f87b487560f
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2022-05-23 13:59:19 +0200

    Implement static route for images

--- src/app/app.js
+++ src/app/app.js
@@ -26,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static(process.env.FRONTEND || 'static'));
 // If process.env.FRONTEND folder does not contain index.html then use the one from static
 app.use('/', express.static('static'));
+app.use('/img', express.static('app/files'));
 
 app.use((req,res,next) => {
     console.log(req.method + ' ' + req.url)
