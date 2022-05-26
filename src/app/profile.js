677b92245d52344c44aa98a96427dc1007b12d1d
IvanValentini <34308333+IvanValentini@users.noreply.github.com>
2022-05-26 11:06:35 +0200

    User API

--- src/app/profile.js
+++ src/app/profile.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const User = require('./models/user.js');
+const tokenChecker = require('./tokenChecker');
 
 
-router.get('', (req, res) => {
+router.get('/me', tokenChecker, (req, res) => {
+    if(!req.loggedUser) {
+        res.status(400).json({ error: 'You have to login!' });
+        return;
+    }
+    res.sendFile(path.join(__dirname,'../static/profile.html'));
+});
+
+router.get('/:username', async (req, res) => {
+    if(!req.params.username){
+        res.status(400).json({ error: 'You have to specify a username!' });
+        return;
+    }
+    
+    // https://mongoosejs.com/docs/api.html#model_Model.find
+    let user = await User.findOne({username: req.params.username});
+    if(!user){
+        res.status(400).json({ error: 'User not found!' });
+        return;
+    }
     res.sendFile(path.join(__dirname,'../static/profile.html'));
 });
 
