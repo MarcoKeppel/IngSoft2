fa87920c43eca657046181188e5023bbf4756e3c
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2022-05-30 17:07:27 +0200

    Use DB_URL env var in app/upload.test.js

--- app/upload.test.js
+++ app/upload.test.js
@@ -9,7 +9,7 @@ describe('POST /api/v1/upload', () => {
 
     beforeAll( async () => {
         jest.setTimeout(8000);
-        app.locals.db = await mongoose.connect(/*process.env.DB_URL*/ 'mongodb://localhost:27017/Discoveroo');
+        app.locals.db = await mongoose.connect(process.env.DB_URL);
     });
     afterAll( () => {
         mongoose.connection.close(true);
