cdca945e4cc07f23a8684052a1a8dde446d684ed
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2022-05-31 17:19:10 +0200

    Fix test cases Add more test cases

--- app/upload.test.js
+++ app/upload.test.js
@@ -18,7 +18,50 @@ describe('POST /api/v1/upload', () => {
     test('POST /api/v1/upload without authentication', () => {
         return request(app).post('/api/v1/upload')
         //.set('Accept', 'application/json')
+        .field("title", "sample title")
         .expect(401, { success: false, message: "No token provided." });
+    });
+
+    test('POST /api/v1/upload with authentication, without submitting a file', async () => {
+
+        let user = await User.findOne({
+            email: "mail@example.com"
+        }).exec();
+        let payload = {
+            email: user.email,
+            id: user._id,
+            username: user.username	
+        }
+        let options = {
+            expiresIn: 86400
+        }
+        let token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+
+        return request(app).post('/api/v1/upload')
+        .set('x-access-token', token)
+        .field("title", "sample title")
+        .expect(400, "No files were uploaded.");
+    });
+
+    test('POST /api/v1/upload with authentication, without providing a title', async () => {
+
+        let user = await User.findOne({
+            email: "mail@example.com"
+        }).exec();
+        let payload = {
+            email: user.email,
+            id: user._id,
+            username: user.username	
+        }
+        let options = {
+            expiresIn: 86400
+        }
+        let token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+
+        return request(app).post('/api/v1/upload')
+        .set('x-access-token', token)
+        .attach("myFile", path.resolve(__dirname, './upload.js'))
+        .expect(400, "A title for the post must be provided");
     });
 
     test('POST /api/v1/upload with authentication, submitting a non-image file', async () => {
@@ -26,42 +69,101 @@ describe('POST /api/v1/upload', () => {
         let user = await User.findOne({
             email: "mail@example.com"
         }).exec();
-        var payload = {
+        let payload = {
             email: user.email,
             id: user._id,
             username: user.username	
         }
-        var options = {
+        let options = {
             expiresIn: 86400
         }
-        var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+        let token = jwt.sign(payload, process.env.SUPER_SECRET, options);
 
         return request(app).post('/api/v1/upload')
         .set('x-access-token', token)
-        .attach("myFile", path.resolve(__dirname, './upload.js'), { contentType: 'multipart/form-data' })
+        .field("title", "sample title")
+        .attach("myFile", path.resolve(__dirname, './upload.js'))
         .expect(415, "Invalid file type provided");
     });
+
+    let postId;
 
     test('POST /api/v1/upload with authentication, submitting an image file', async () => {
 
         let user = await User.findOne({
             email: "mail@example.com"
         }).exec();
-        var payload = {
+        let payload = {
             email: user.email,
             id: user._id,
             username: user.username	
         }
-        var options = {
+        let options = {
             expiresIn: 86400
         }
-        var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+        let token = jwt.sign(payload, process.env.SUPER_SECRET, options);
 
         const buffer = Buffer.from(path.resolve(__dirname, "./files/628b7f93785eab9840743db7.png"));
 
-        return request(app).post('/api/v1/upload')
+        const response = await request(app).post('/api/v1/upload')
         .set('x-access-token', token)
-        .attach("myFile", buffer, "image.png")
-        .expect(200);
+        .field("title", "sample title")
+        .attach("myFile", buffer, "image.png");
+
+        expect(response.statusCode).toBe(302);
+        expect(response.header.location).toMatch(/^\/post\/[0-9a-fA-F]{24}$/);
+
+        postId = response.header.location.split("/").pop();
+    });
+
+    // TODO
+    test('POST /api/v1/upload/:postID with authentication, invalid postID', async () => {
+
+        let user = await User.findOne({
+            email: "mail@example.com"
+        }).exec();
+        let payload = {
+            email: user.email,
+            id: user._id,
+            username: user.username	
+        }
+        let options = {
+            expiresIn: 86400
+        }
+        let token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+
+        const response = await request(app).get('/api/v1/upload/000000000000000000000000')
+        .set('x-access-token', token);
+
+        expect(response.statusCode).toBe(400);
+    });
+
+    test('POST /api/v1/upload/:postID with authentication, valid postID', async () => {
+
+        let user = await User.findOne({
+            email: "mail@example.com"
+        }).exec();
+        let payload = {
+            email: user.email,
+            id: user._id,
+            username: user.username	
+        }
+        let options = {
+            expiresIn: 86400
+        }
+        let token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+
+        const response = await request(app).get('/api/v1/upload/' + postId)
+        .set('x-access-token', token);
+        console.log(response.body)
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('title', expect.any(String));
+        expect(response.body).toHaveProperty('votes', expect.any(Object));
+        expect(response.body).toHaveProperty('user', expect.any(Object));
+        expect(response.body.user).toHaveProperty('username', expect.any(String));
+        expect(response.body).toHaveProperty('comments', expect.any(Array));
+        expect(response.body).toHaveProperty('picture_name', expect.any(String));
+        expect(response.body).toHaveProperty('picture_path', expect.any(String));
+        expect(response.body).toHaveProperty('time', expect.any(Number));
     });
 });
