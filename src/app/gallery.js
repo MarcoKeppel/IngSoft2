6f16abbb8eb27c12a83b5875f9de19470f31b567
IvanValentini <34308333+IvanValentini@users.noreply.github.com>
2022-05-31 12:43:38 +0200

    /post endpoint

--- src/app/gallery.js
+++ src/app/gallery.js
@@ -6,7 +6,7 @@ const tokenChecker = require('./tokenChecker.js');
 
 router.get("", async (req, res) => {
 	// Get all users from DB
-	let users = await User.find().select('email posts -_id').populate('posts', 'time path -_id').lean();
+	let user = await User.findOne({username: req.params.username}).select('email posts -_id').populate('posts', 'title').lean();
 
 	posts = [];
 
@@ -30,7 +30,7 @@ router.get("/me", tokenChecker, async (req, res) => {
         return;
     }
 
-	let user = await User.findOne({_id: req.loggedUser.id}).select('email posts -_id').populate('posts', 'time path -_id').lean();
+	let user = await User.findOne({username: req.params.username}).select('email posts -_id').populate('posts', 'title').lean();
 
 	let posts = user.posts;
 	posts.sort( (a, b) => {
@@ -47,13 +47,12 @@ router.get("/:username", async (req, res) => {
     }
 
 	// Get all users from DB
-	let user = await User.findOne({username: req.params.username}).select('email posts -_id').populate('posts', 'time path -_id').lean();
+	let user = await User.findOne({username: req.params.username}).select('email posts -_id').populate('posts', 'title').lean();
 
 	if(!user){
 		res.status(400).json({ error: 'User not found!' });
 		return;
 	}
-	
 	let posts = user.posts;
 	posts.sort( (a, b) => {
 		return b.time - a.time;
--- src/app/upload.js
+++ src/app/upload.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Post = require('./models/post.js'); // get our mongoose model
 const User = require('./models/user.js'); // get our mongoose model
+const Comment = require('./models/comment.js'); // get our mongoose model
 const path_module = require('path');
+const tokenChecker = require('./tokenChecker.js');
 
-router.post("/", async (req, res) => {
+router.post("/", tokenChecker, async (req, res) => {
     if (!req.files) {
       return res.status(400).send("No files were uploaded.");
     }
@@ -58,13 +60,64 @@ router.post("/", async (req, res) => {
         if (err) {
           return res.status(500).send(err);
         }
-        return res.send({ status: "success", path: path });
+        return res.redirect('/post/' + post._id);
       });
     }
     else
     {
       return res.status(400).send("No user found");
     }
+});
+
+router.get('/:postID', async (req, res) => {  
+  if(!req.params.postID){
+      res.status(400).json({ error: 'You have to specify a post ID!' });
+      return;
+  }
+  
+  // https://mongoosejs.com/docs/api.html#model_Model.find
+  let post = await Post.findOne({_id: req.params.postID});
+  if(!post){
+      res.status(400).json({ error: 'Post not found!' });
+      return;
+  }
+
+  // This sould not be necessary, but let's check it anyway
+  let user = await User.findOne({_id: post.user});
+  if(!user){
+      res.status(400).json({ error: 'User not found!' });
+      return; 
+  }
+
+  let comments = [];
+  for(let i = 0; i < post.comments.length; i++){
+    let comment = await Comment.findOne({_id: post.comments[i]});
+    if(comment){
+      let userComment = await User.findOne({_id: comment.user._id});
+      let tmp_comment = {};
+      tmp_comment.text = comment.text;
+      tmp_comment.votes = comment.votes;
+      tmp_comment.user = {
+        username : userComment.username,
+      };
+      tmp_comment.time = comment.time;
+      comments.push(tmp_comment);
+    }
+  }
+  console.log(comments);
+
+  res.status(200).json({
+    title: post.title,
+    text: post.text,
+    votes: post.votes,
+    user: {
+      username: user.username,      
+    },
+    comments: comments,
+    picture_name: post.picture_name,
+    picture_path: post.picture_path,
+    time: post.time
+  });
 });
 
 module.exports = router;
