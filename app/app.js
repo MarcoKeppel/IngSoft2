bcc3061914cb9bf55d62f7555c749d8e6a464b7d
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2022-06-07 13:45:00 +0200

    Add /image API route

--- app/app.js
+++ app/app.js
@@ -16,6 +16,7 @@ const profile = require('./profile.js');
 const follow = require('./follow.js');
 const comment = require('./comment.js');
 const vote = require('./vote.js');
+const image = require('./image.js');
 
 app.use(express.json());
 app.use(cookieParser());
@@ -34,7 +35,7 @@ app.use(cors());
 app.use('/', express.static(process.env.FRONTEND || 'static'));
 // If process.env.FRONTEND folder does not contain index.html then use the one from static
 app.use('/', express.static('static'));
-app.use('/img', express.static('app/files'));
+//app.use('/img', express.static('app/files'));
 
 app.use((req,res,next) => {
     console.log(req.method + ' ' + req.url)
@@ -48,9 +49,11 @@ app.use('/api/v1/upload', upload);
 app.use('/api/v1/follow', tokenChecker, follow);
 app.use('/api/v1/comment', tokenChecker, comment);
 app.use('/api/v1/gallery', gallery);
+app.use('/api/v1/vote', tokenChecker, vote);
+app.use('/api/v1/image', image);
+
 app.use('/profile', profile);
 app.use('/post', post);
-app.use('/api/v1/vote', tokenChecker, vote);
 
 
 app.use((req, res) => {
